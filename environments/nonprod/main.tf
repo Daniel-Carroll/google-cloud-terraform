6ac1332c172b58471c9d008f41917178107dccd6
Daniel-Carroll <danielcarroll95@gmail.com>
2023-10-07 18:42:52 -0500

    update module path

--- environments/nonprod/main.tf
+++ environments/nonprod/main.tf
@@ -7,19 +7,19 @@ provider "google" {
 }
 
 module "vpc" {
-  source  = "../../modules/vpc"
+  source  = "../../modules/google/vpc"
   project = var.project
   env     = local.env
 }
 
 module "http_server" {
-  source  = "../../modules/http_server"
+  source  = "../../modules/google/http_server"
   project = var.project
   subnet  = module.vpc.subnet
 }
 
 module "firewall" {
-  source  = "../../modules/firewall"
+  source  = "../../modules/google/firewall"
   project = var.project
   subnet  = module.vpc.subnet
 }
